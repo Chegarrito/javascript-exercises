0355ca904f6a54993e1b0d18c1047dec7b8f5b5b
Chegarrito <119348988+Chegarrito@users.noreply.github.com.>
2024-06-27 00:07:37 -0500

    Pass the first two tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,24 @@
-const removeFromArray = function(array, remove) {
-    let arrayLength = array.length;
-    for (let i = 0; i < arrayLength; i++){
-        if (array[i] === remove){
-            array.splice(i, 1);
+const removeFromArray = function(...param) {
+    // param[0] equals to the array
+    let array = param[0];
+    let numberOfParameters = param.length;
+    // array for the parameters
+    let values = [];
+    // assign the values of the parameters to an array
+    // this is correct
+    for (let i = 1; i < numberOfParameters; i++){
+        values[i - 1] = param[i]; 
+    }
+    // Loop thru the values
+    for(let i = 0; i < values.length; i++){
+        // Loop thru the array
+        for (let j = 0; j < array.length; j++){
+            if(values[i] === array[j]){
+                array.splice(j, 1);
+            }
         }
     }
+// npm test removeFromArray.spec.js
     return array;
 
 
