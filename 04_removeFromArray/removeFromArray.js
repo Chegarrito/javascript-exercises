90a3d9b6c925058814c840bcf9589277ae392f82
Chegarrito <119348988+Chegarrito@users.noreply.github.com.>
2024-06-26 22:43:47 -0500

    Pass the first test

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    let arrayLength = array.length;
+    for (let i = 0; i < arrayLength; i++){
+        if (array[i] === remove){
+            array.splice(i, 1);
+        }
+    }
+    return array;
+
+
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
